694d090848d5e3fd9425b85f0530b32c6ff2bf50
irepnov <irepnov@gmail.com>
2020-06-09 14:55:13 +0300

    fgd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,72 +1,140 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
-    class A
+    public class A
     {
         public virtual void Start() => Console.WriteLine("A.start");
         public virtual void Work() => Console.WriteLine("A.work");
     }
-    class B : A
+    public class B : A
     {
         public new void Start() => Console.WriteLine("B.start");
         public override void Work() => Console.WriteLine("B.work");
     }
-    class C : B
+    public class C : B
     {
         public void Start() => Console.WriteLine("C.start");
         public override void Work() => Console.WriteLine("C.work");
+        public void Finish() => Console.WriteLine("C.finish");
     }
 
-    class Program
+  //  [XmlInclude(typeof(Child))]
+    public class Parent
     {
-        static void metodWork(A _in)
+        public string firstname;
+        public string lastname;
+    }
+    public class Child: Parent
+    {
+        public string fullname;
+    }
+
+    
+    [XmlRoot("Objects")]
+    [XmlInclude(typeof(Parent))]
+    [XmlInclude(typeof(Child))]
+    public class Group
+    {
+        public int num;            
+        public Parent[] objects;
+    }
+
+    public class Program
+    {
+        public static void metodWork(A _in)
         {
             Console.WriteLine("metod Work");
             _in.Work();
         }
-        static void metodWork(B _in)
+        public static void metodWork(B _in)
         {
             Console.WriteLine("metod Work");
             _in.Work();
         }
-        static void metodWork(C _in)
+        public static void metodWork(C _in)
         {
             Console.WriteLine("metod Work");
             _in.Work();
         }
-        static void metodStart(B _in)
+        public static void metodStart(B _in)
+        {
+            Console.WriteLine("metod Start");
+            _in.Start();
+        }
+        public static void metodStart(A _in)
+        {
+            Console.WriteLine("metod Start");
+            _in.Start();
+        }
+        public static void metodStart(C _in)
         {
             Console.WriteLine("metod Start");
             _in.Start();
         }
 
-        static void metodStart(A _in)
+
+        public void SerializeObject(string filename)
         {
-            Console.WriteLine("metod Start");
-            _in.Start();
+            // Creates a new XmlSerializer.
+            XmlSerializer s = new XmlSerializer(typeof(Group));
+
+            // Writing the XML file to disk requires a TextWriter.
+            TextWriter writer = new StreamWriter(filename);
+            Group group = new Group();
+
+            Parent manager = new Parent();
+            Child emp1 = new Child();
+            Child emp2 = new Child();
+            manager.firstname = "fir";
+            manager.lastname = "las";
+            emp1.firstname = "fir1";
+            emp1.lastname = "las1";
+            emp1.fullname = "full1";
+
+            emp2.firstname = "fir2";
+            emp2.fullname = "full2";
+            Parent[] emps = new Parent[3] { manager, emp1, emp2 };
+            group.num = emps.Length;
+            group.objects = emps;
+
+            // Serializes the object, and closes the StreamWriter.
+            s.Serialize(writer, group);
+            writer.Close();
         }
 
-        static void metodStart(C _in)
+        public void DeserializeObject(string filename)
         {
-            Console.WriteLine("metod Start");
-            _in.Start();
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            XmlSerializer x = new XmlSerializer(typeof(Group));
+            Group g = (Group)x.Deserialize(fs);
+            Console.WriteLine("Members:");
+
+            foreach (Parent e in g.objects)
+            {
+                Console.WriteLine("\t" + e.firstname + " " + e.ToString());
+            }
         }
 
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Program test = new Program();
+            test.SerializeObject("TypeDoc.xml");
+            test.DeserializeObject("TypeDoc.xml");
+
             C obj = new C();
 
-           // metodWork((A)obj); //будет вызван метод Ворк класса C, т.к. класс А является также классом Б, и в классе Б метод override
-           // metodWork((B)obj); //будет вызван метод Ворк класса C 
-           // metodWork((C)obj); //будет вызван метод Ворк класса c
+            metodWork((A)obj); //будет вызван метод Ворк класса C, т.к. класс А является также классом Б, и в классе Б метод override
+            metodWork((B)obj); //будет вызван метод Ворк класса C 
+            metodWork((C)obj); //будет вызван метод Ворк класса c
 
             metodStart((A)obj); //будет вызван метод Старт класса А, т.к. метод Старт класса C, скрыт от класса А (написано в классе C     public void == public new void)
             metodStart((B)obj); //будет вызван метод Старт класса Б, т.к. метод Старт класса C является новым (скрытым)
